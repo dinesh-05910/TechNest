dec5a5dc2abe9d16eff6cf728e3160e83a049476
dinesh-05910 <chd.05910@gmail.com>
2024-05-14 22:51:52 +0530

    Fixed the code so the data can be loaded from the back-end product.js data file

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,12 +3,22 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const ProductScreen = () => {
 
+  const [product, setProduct] = useState([]);
+
   const {id : productId } = useParams();
-  const product = products.find((p) => p._id === productId); 
+  useEffect(() => {
+    const fetchProduct = async () => {
+        const { data } = await axios.get(`/api/products/${productId}`);
+        setProduct(data);
+        }
+        fetchProduct();
+    }, [productId]);
+  
   return (
     <>
         <Link className='btn btn-light my-3' to='/'>
