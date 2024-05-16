50149f3cc70d003e9baed3194ff3eb62776b72df
dinesh-05910 <chd.05910@gmail.com>
2024-05-16 15:02:52 +0530

    Added the models for Order, user, Product

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config(); 
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 const port = process.env.PORT || 1234;
 
-const app = express()
+connectDB();
+
+const app = express();
 
 app.get('/',(req,res) => {
     res.send('API is running!');
