1834102076029e667f3535dbb2d2c946e68ddebf
dinesh-05910 <chd.05910@gmail.com>
2024-05-16 15:48:25 +0530

    "Updated orderModel.js, added seeder.js, and modified package.json"

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -72,6 +72,6 @@ const orderSchema = new mongoose.Schema({
     timestamps: true,
 })
 
-const Order = mongoose.model("Order","orderSchema");
+const Order = mongoose.model("Order",orderSchema);
 
 export default Order;
