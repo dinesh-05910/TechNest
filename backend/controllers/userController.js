bea0d2a036aed863b70ca0cab7272a8e65b99d20
dinesh-05910 <chd.05910@gmail.com>
2024-05-20 15:48:25 +0530

    "Updated authUser and getUserProfile controllers to return JSON responses with user data, and implemented updateUserProfile controller to update user information."

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async(req,res) => {
 
     if(user && (await user.matchPassword(password))){
         generateToken(res,user._id);
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -75,7 +75,18 @@ const logoutUser = asyncHandler(async(req,res) => {
 // @access Public
 
 const getUserProfile = asyncHandler(async(req,res) => {
-    res.send('User Profile')
+    const user = await User.findById(req.user._id);
+    if(user){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    }else{
+        res.status(404);
+        throw new Error("User Not Found!");
+    }
 });
 
 // @desc Update user profile
@@ -83,7 +94,24 @@ const getUserProfile = asyncHandler(async(req,res) => {
 // @access Private
 
 const updateUserProfile = asyncHandler(async(req,res) => {
-    res.send('Update User Profile')
+    const user = await User.findById(req.user._id);
+    if(user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if(req.body.password){
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    }else{
+        res.status(404);
+        throw new Error("User Not Found!");
+    }
 });
 
 // @desc Get users
