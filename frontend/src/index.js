e94432d03cd543030dfc9ae6057369ee722eb76b
dinesh-05910 <chd.05910@gmail.com>
2024-05-20 23:36:31 +0530

    "Added PaymentScreen route and savePaymentMethod action to cartSlice"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
       {/* Private Routes */}
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>
   )
