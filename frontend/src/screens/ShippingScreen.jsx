4129b808026328b3cd8187ae64829ff8e3153c06
dinesh-05910 <chd.05910@gmail.com>
2024-05-20 23:12:10 +0530

    "Added CheckoutSteps component to ShippingScreen"

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../slices/cartSlice'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const ShippingScreen = () => {
 
@@ -28,6 +29,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping Address</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='address' className='my-2'>
