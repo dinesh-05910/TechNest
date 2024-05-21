619899a32a6cce7c29862d988edee12a946ed096
dinesh-05910 <chd.05910@gmail.com>
2024-05-21 21:21:23 +0530

    "Added admin dropdown menu to Header component and implemented AdminRoute with OrderListScreen"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,8 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import AdminRoute from './components/AdminRoute';
+import OrderListScreen from './screens/admin/OrderListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -43,6 +45,11 @@ const router = createBrowserRouter(
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/order/:id" element={<OrderScreen />} />
         <Route path="/profile" element={<ProfileScreen/>} />
+      </Route>
+
+      {/* AdminRoute */}
+      <Route path="" element={<AdminRoute />}>
+        <Route path="/admin/orderlist" element={<OrderListScreen />} />
       </Route>
     </Route>
   )
---
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrderListScreen = () => {
+  return (
+    <div>OrderListScreen</div>
+  )
+}
+
+export default OrderListScreen
