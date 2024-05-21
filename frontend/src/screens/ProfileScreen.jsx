de2fc8369edbdd09fe26cc405d3cd5b81488ce32
dinesh-05910 <chd.05910@gmail.com>
2024-05-21 19:36:23 +0530

    "Added My Orders section to ProfileScreen, fetching orders data with useGetMyOrdersQuery and displaying it in a table, and created useGetMyOrdersQuery in ordersApiSlice"

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -6,9 +6,10 @@ import { Row, Col, Table, Form, Card, Button } from 'react-bootstrap';
 import { toast } from "react-toastify";
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import { FaTimes } from 'react-icons/fa';
 import { useProfileMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
-
+import { useGetMyOrdersQuery } from '../slices/ordersApiSlice';
 
 const ProfileScreen = () => {
   
@@ -22,6 +23,7 @@ const ProfileScreen = () => {
   const { userInfo } = useSelector((state) => state.auth);
 
   const [updateProfile, {isLoading:loadingUpdateProfile}] = useProfileMutation();
+  const {data:orders, isLoading, error} = useGetMyOrdersQuery();
 
   useEffect(() => {
     if(userInfo){
@@ -91,7 +93,46 @@ const ProfileScreen = () => {
                 { loadingUpdateProfile && <Loader />}
             </Form>
         </Col>
-        <Col md={9}>Column</Col>
+        <Col md={9}>
+            <h2>My Orders</h2>
+            {isLoading? <Loader/> : error? (
+                <Message>{error?.data?.message || error.error}</Message>
+            ) : (
+                <Table striped bordered hover responsive className='table-sm'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>DATE</th>
+                            <th>TOTAL</th>
+                            <th>PAID</th>
+                            <th>DELIVERED</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order) => (
+                            <tr key={order._id}>
+                                <td>{order._id}</td>
+                                <td>{order.createdAt.substring(0,10)}</td>
+                                <td>{order.totalPrice}</td>
+                                <td>{order.isPaid? order.paidAt.substring(0,10): (
+                                    <FaTimes style={{color: 'red'}} />
+                                )}</td>
+                                <td>{order.isDelivered? order.deliveredAt.substring(0,10): (
+                                    <FaTimes style={{color: 'red'}} />
+                                )}</td>
+                                <td>
+                                    <LinkContainer to={`/order/${order._id}`}>
+                                        <Button variant='light' className='btn-sm'>
+                                            Details
+                                        </Button>
+                                    </LinkContainer>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </Col>
     </Row>
   )
 }
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -29,8 +29,14 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5,
         }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/myorders`,
+            }),
+            keepUnusedDataFor: 5,
+        })
     }),  
 });
 
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery } = ordersApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery, useGetMyOrdersQuery } = ordersApiSlice;
