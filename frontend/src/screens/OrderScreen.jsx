645ef869efbacaabf95a74daaec5eb48ebb3cbaf
dinesh-05910 <chd.05910@gmail.com>
2024-05-21 17:26:12 +0530

    Remove Form import from react-bootstrap in OrderScreen.jsx

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { Row, Col, ListGroup, Image, Button, Card } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
 import Message from '../components/Message'
