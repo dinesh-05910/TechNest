0f1c7d1bae12129fb03d52a1998d7eea9bccff8c
dinesh-05910 <chd.05910@gmail.com>
2024-05-23 02:42:37 +0530

    "Updated ProductCarousel component: changed background color from 'primary' to 'dark', and commented out image style attributes."

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -8,11 +8,11 @@ const ProductCarousel = () => {
 
   const {data: products, isLoading, error} = useGetTopProductsQuery();
   return isLoading? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-    <Carousel pause='hover' className='bg-primary mb-4'>
+    <Carousel pause='hover' className='bg-dark mb-4'>
         {products.map(product => (
             <Carousel.Item key={product._id}>
                 <Link to={`/product/${product._id}`}>
-                    <Image src={product.image} alt={product.name} fluid />
+                    <Image /*style={{height:'500px', width:'625px'}}*/ src={product.image} alt={product.name} fluid />
                     <Carousel.Caption className='carousel-caption'>
                         <h2>{product.name} (${product.price})</h2>
                     </Carousel.Caption>
