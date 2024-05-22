2cd04154636832771db5f3cd0c24d3321e0af7a6
dinesh-05910 <chd.05910@gmail.com>
2024-05-22 17:30:31 +0530

    "Added product review feature: created createProductReview function in productController.js and added corresponding route in productRoutes.js"

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import {createProduct, getProducts, getProductsById, updateProduct, deleteProduct} from '../controllers/productController.js';
+import {createProduct, getProducts, getProductsById, updateProduct, deleteProduct, createProductReview} from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect,admin,createProduct);
-router.route('/:id').get(getProductsById).put(protect,admin,updateProduct).delete(protect,admin,deleteProduct);
+router
+    .route('/:id')
+    .get(getProductsById)
+    .put(protect,admin,updateProduct)
+    .delete(protect,admin,deleteProduct)
+router.route('/:id/reviews').post(protect,createProductReview);
 
 export default router;
