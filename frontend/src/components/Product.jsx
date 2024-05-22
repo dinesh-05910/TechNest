ab9a25bd40e5f77a232ee5daa46a57a8b53e8bb4
dinesh-05910 <chd.05910@gmail.com>
2024-05-23 02:22:12 +0530

    "Updated favicon.ico, added a newline in bootstrap.custom.css, and modified Card.Img in Product.jsx to include a height style"

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,7 +7,7 @@ const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
         <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top' />
+            <Card.Img style={{height:'200px'}} src={product.image} variant='top' />
         </Link>
 
         <Card.Body>
