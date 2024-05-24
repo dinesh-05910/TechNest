98ef8090f72c1fa482f7dcb96df949d67347d0be
dinesh-05910 <chd.05910@gmail.com>
2024-05-24 16:33:26 +0530

    "Updated product controller to use environment variable for pagination limit, removed root route from server, added production environment setup for frontend, and updated package.json scripts"

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ import Product from "../models/productModel.js";
 // @access Public
 
 const getProducts = asyncHandler(async(req,res) => {
-    const pageSize = 8;
+    const pageSize = process.env.PAGINATION_LIMIT;
     const page = Number(req.query.pageNumber) || 1;
 
     const keyword = req.query.keyword ? {name: { $regex: req.query.keyword, $options: "i" }} : {};
--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: true}));
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/',(req,res) => {
-    res.send('API is running!');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
@@ -36,8 +32,23 @@ app.use('/api/config/paypal',(req,res) => res.send({
     clientId: process.env.PAYPAL_CLIENT_ID
 }))
 
- const __dirname = path.resolve(); //Set __dirname to current directory
- app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+const __dirname = path.resolve(); //Set __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    //any route that is not api will be redirected to index.html
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}else{
+    app.get('/',(req,res) => {
+        res.send('API is running!');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
